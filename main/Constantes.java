2f473a20fac34811aca535b1b6de087a00778929
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-20 23:52:01 -0600

    Creé constantes.java

---
+++ main/Constantes.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Constantes {
+	public static void main(String[] args) {
+		
+		//Para declarar una constante solamente le agregamos al principio la palabra "final"
+		//con esto, no se podra modificar el valor de esa constante en todo el código
+		
+		final double pi = 3.1416, e = 3.14f;
+		
+		System.out.println(pi);
+		System.out.print(e);
+	}
+}
