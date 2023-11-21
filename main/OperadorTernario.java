2d3eb752738f3d22b0e01de420d49f74062041ba
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-21 16:06:46 -0600

    Añadí el programa OperadorTernario

---
+++ main/OperadorTernario.java
@@ -0,0 +1,12 @@
+package main;
+
+public class OperadorTernario {
+		public static void main(String[] args) {
+			// si expresión 1 true entonces expresion 2
+			// si expresión 1 fale entonces expresión 3
+			
+			String resultado = 10 > 5 ? "Es mayor": "Es menor";
+			System.out.println(resultado);
+			
+		}
+}
