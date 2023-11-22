bfd9660b3a69a8ee14ee1b9ad3e8f02675563716
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-21 23:15:36 -0600

    Añadí el programa Condicionales.java

--- main/ComparacionStrings.java
+++ main/ComparacionStrings.java
@@ -1,17 +1,18 @@
 package main;
 
 public class ComparacionStrings {
-		public static void main(String[] args) {
-			
-			String string1 = "HOLA";
-			String string2 = "hola";
-			
-			// Con equals comparamos el objeto con otro para ver si son iguales
-			boolean resultado = (string1.toUpperCase()).equals(string2.toUpperCase());
-			System.out.println(resultado);
-			
-			// Y con este comando se ignora si son mayusculas o minusculas para hacer la comparación.
-			resultado = string1.equalsIgnoreCase(string2);
-			System.out.println(resultado);
-		}
+	public static void main(String[] args) {
+
+		String string1 = "HOLA";
+		String string2 = "hola";
+
+		// Con equals comparamos el objeto con otro para ver si son iguales
+		boolean resultado = (string1.toUpperCase()).equals(string2.toUpperCase());
+		System.out.println(resultado);
+
+		// Y con este comando se ignora si son mayusculas o minusculas para hacer la
+		// comparación.
+		resultado = string1.equalsIgnoreCase(string2);
+		System.out.println(resultado);
+	}
 }
