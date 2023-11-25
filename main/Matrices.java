6c1882e624f30b4f45236b195d17a28f460449c0
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-25 00:01:29 -0600

    Creé Matrices.java

---
+++ main/Matrices.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Matrices {
+	public static void main(String[] args) {
+		
+		int matriz[][] = new int [4][3];
+		
+		matriz[0][0] = 1;
+		matriz[0][1] = 2;
+		matriz[0][2] = 3;
+		
+		matriz[1][0] = 1;
+		matriz[1][1] = 2;
+		matriz[1][2] = 3;
+	
+		matriz[2][0] = 1;
+		matriz[2][1] = 2;
+		matriz[2][2] = 3;
+		
+		matriz[3][0] = 1;
+		matriz[3][1] = 2;
+		matriz[3][2] = 3;
+		
+		System.out.println(matriz[0][0]);
+	}
+	
+	
+}
