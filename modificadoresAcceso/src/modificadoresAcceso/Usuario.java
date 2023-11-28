61d5b3f3c9f875f3c93fd83049cf1eb73678bd3d
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 15:35:26 -0600

    Añadí contenido a usuario.java

--- modificadoresAcceso/src/modificadoresAcceso/Usuario.java
+++ modificadoresAcceso/src/modificadoresAcceso/Usuario.java
@@ -1,5 +1,15 @@
 package modificadoresAcceso;
 
 public class Usuario {
-
+	public String username;
+	private String password;
+	
+	//Default
+	//Public
+	//Private
+	//Protected
+	
+	public void saluda() {
+		System.out.println("Hola, mi username es " + this.username);
+	}
 }
