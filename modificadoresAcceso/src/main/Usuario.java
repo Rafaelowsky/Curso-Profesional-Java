eaef807651540ff5ed154a92c285a49ba91025c2
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 15:59:07 -0600

    Mejore comentarios sobre tipos de encapsulamiento

--- modificadoresAcceso/src/main/Usuario.java
+++ modificadoresAcceso/src/main/Usuario.java
@@ -5,10 +5,15 @@ public class Usuario {
 	private String password; // Esta variable solo es accesible dentro de esta clase
 	
 	//Estos son los tipos de encapsulamientos que hay:
-	//Default
-	//Public
-	//Private
-	//Protected
+	
+	// private: Accesible solo dentro de la misma clase.
+	
+	// default: Accesible solo dentro del mismo paquete.
+	
+	// protected: Accesible dentro del mismo paquete y por subclases, incluso si están en paquetes diferentes.
+
+	// public: Accesible desde cualquier clase.
+
 	
 	public void saluda() {
 		System.out.println("Hola, mi username es " + this.username);
