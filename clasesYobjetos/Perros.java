a19411a0829b7cef65d0d1a36f1d0990f3fb672f
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-27 22:04:38 -0600

    Creé clase Perros.java

---
+++ clasesYobjetos/Perros.java
@@ -0,0 +1,14 @@
+package clasesYobjetos;
+
+// Así podemos inicializar una clase
+// Las clases son como moldes para crear objetos
+public class Perros {
+	
+	// Cada clase tiene sus caracteristicas
+	// Tales como atributos y métodos
+	
+	String nombre;
+	String raza;
+	int edad;
+	
+}
