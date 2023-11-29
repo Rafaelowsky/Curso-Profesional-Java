1d73d88807fa4191c8aeedeb14d03deb4eceea0d
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 11:54:45 -0600

    Añadí super en método dormir de clase Perro y Gato

--- Herencia/src/main/Gato.java
+++ Herencia/src/main/Gato.java
@@ -13,16 +13,22 @@ public class Gato extends Mascota {
 		this.raza = raza;
 		this.edad = edad;
 	}
-	
-	/* El polimorfismo en Java permite a objetos de distintas clases relacionadas
-	   ser tratados de manera uniforme a través de una interfaz común o clase base.
-	   Esto se logra al llamar a métodos sobre un objeto, donde la implementación
-	   específica de ese método se determina en tiempo de ejecución según el tipo
-	   real del objeto. Esto promueve la reutilización de código y la flexibilidad
-	   al trabajar con distintas clases que comparten una interfaz o herencia. */
 
-	@Override 
+	/*
+	 * El polimorfismo en Java permite a objetos de distintas clases relacionadas
+	 * ser tratados de manera uniforme a través de una interfaz común o clase base.
+	 * Esto se logra al llamar a métodos sobre un objeto, donde la implementación
+	 * específica de ese método se determina en tiempo de ejecución según el tipo
+	 * real del objeto. Esto promueve la reutilización de código y la flexibilidad
+	 * al trabajar con distintas clases que comparten una interfaz o herencia.
+	 */
+
+	// "Sobre-escritura de método"
+	@Override
 	void dormir() {
+		// Si no queremos perder funcionalidades de la clase padre ocupamos super
+
+		super.dormir(); // Dormir() de clase padre
 		System.out.println("El gato se encuentra durmiendo");
 	}
 }
