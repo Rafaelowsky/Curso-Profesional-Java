3be20c16eaec51843164e7522678e1186fcfa3ad
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 23:59:46 -0600

    Comprobé en main que la clase anidada funciona

---
+++ clasesAnidadas/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		//Instanciamos un usuario con el constructor
+		Usuario Usuario1 = new Usuario("Rafael");
+		
+		Usuario1.establecerRol();
+	}
+}
