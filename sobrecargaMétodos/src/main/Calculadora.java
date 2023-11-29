33bb96c187ae7f8d3e25f338af820f08a70f8627
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 21:04:36 -0600

    Añadí comentarios

--- sobrecargaMétodos/src/main/Calculadora.java
+++ sobrecargaMétodos/src/main/Calculadora.java
@@ -2,6 +2,9 @@ package main;
 
 public class Calculadora {
 
+	// "Sobrecarga de métodos": Múltiples métodos con el mismo nombre en una clase
+	// pero con diferentes parámetros.
+
 	// Enteros
 	public int sumar(int x, int y) {
 		return x + y;
