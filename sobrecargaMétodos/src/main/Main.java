26dfcd71eb60729c2e0bb33e8c4373907a3d160f
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 21:02:12 -0600

    Comprobé método sumar en main.java

--- sobrecargaMétodos/src/main/Main.java
+++ sobrecargaMétodos/src/main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+	
+	Calculadora calculadora = new Calculadora();
+	
+	System.out.println(calculadora.sumar(1.5, 1.6));
+}
 }
