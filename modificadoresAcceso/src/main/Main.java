0d539824a1e1c4db31aad0e23a5b49c2b903d5d6
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 20:11:42 -0600

    Mejoré código en instancia y comentarios

--- modificadoresAcceso/src/main/Main.java
+++ modificadoresAcceso/src/main/Main.java
@@ -4,15 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Usuario Alex = new Usuario();
-		Alex.username = "Alexis";
-		// Entonces la siguiente linea de código no compilaria ya que no es posible
-		// acceder
-		// al atributo solicitado
+		// Gracias al constructor esto paso de estas 3 lineas de codigo a solo 1:
+
+		// Usuario Alex = new Usuario();
+		// Alex.username = "Alexis";
 		// Alex.pasword = "12345";
 
+		Usuario Alex = new Usuario("Alexis", "");
+
+		// Como password es de tipo private entonces no se puede acceder
+		// entonces la siguiente linea de código no compilaria ya que no es posible
+		// acceder
+		// al atributo solicitado
+
+		// Entonces aplicamos un setter y getter para poder trabajar con password
+
+		// Setter
 		Alex.setPassword("12345");
 
+		// Getter
 		if (Alex.getPassword() == "12345") {
 			Alex.saluda();
 		}
--- modificadoresAcceso/src/main/Usuario.java
+++ modificadoresAcceso/src/main/Usuario.java
@@ -1,37 +1,37 @@
 package main;
 
 public class Usuario {
-	
+
 	public String username;
 	private String password; // Esta variable solo es accesible dentro de esta clase
-	
+
 	// "Constructor"
-	public Usuario (String username, String password) {
-		this.username = username; 
+	public Usuario(String username, String password) {
+		this.username = username;
 		this.password = password;
 	}
-	
-	//Estos son los tipos de encapsulamientos que hay:
-	
+
+	// Estos son los tipos de encapsulamientos que hay:
+
 	// private: Accesible solo dentro de la misma clase.
-	
+
 	// default: Accesible solo dentro del mismo paquete.
-	
-	// protected: Accesible dentro del mismo paquete y por subclases, incluso si están en paquetes diferentes.
+
+	// protected: Accesible dentro del mismo paquete y por subclases, incluso si
+	// están en paquetes diferentes.
 
 	// public: Accesible desde cualquier clase.
 
-	
 	public void saluda() {
 		System.out.println("Hola, mi username es " + this.username);
 	}
 
-	//Getter
+	// Getter
 	public String getPassword() {
 		return password;
 	}
 
-	//Setter
+	// Setter
 	public void setPassword(String password) {
 		this.password = password;
 	}
