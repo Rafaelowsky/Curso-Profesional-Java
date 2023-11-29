f58e41fb26fdfaacc5c0d5a0b217a71399ba780a
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 20:56:27 -0600

    Añadí la clase Calculadora

---
+++ sobrecargaMétodos/src/main/Calculadora.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Calculadora {
+
+	// Enteros
+	public int sumar(int x, int y) {
+		return x + y;
+	}
+
+	// Mas de dos enteros
+	public int sumar(int x, int y, int z) {
+		return x + y;
+	}
+
+	// Flotantes
+	public float sumar(float x, float y) {
+		return x + y;
+	}
+
+	// Doubles
+	public double sumar(double x, double y) {
+		return x + y;
+	}
+
+	// Default
+	public int sumar() {
+		return 0;
+	}
+
+}
