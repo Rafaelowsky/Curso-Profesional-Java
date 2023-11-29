9abda2e52f98f1c4f26e1852d2443870d3e2892b
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-28 21:51:10 -0600

    Añadí métodos para múltiples sumas

--- sobrecargaMétodos/src/main/Calculadora.java
+++ sobrecargaMétodos/src/main/Calculadora.java
@@ -5,24 +5,60 @@ public class Calculadora {
 	// "Sobrecarga de métodos": Múltiples métodos con el mismo nombre en una clase
 	// pero con diferentes parámetros.
 
-	// Enteros
-	public int sumar(int x, int y) {
-		return x + y;
+	/*
+	 * Enteros public int sumar(int x, int y) { return x + y; }
+	 * 
+	 * Mas de dos enteros public int sumar(int x, int y, int z) { return x + y; }
+	 */
+
+	// Multiples parámetros enteros
+
+	public int sumar(int... numeros) {
+		int suma = 0;
+
+		System.out.println(numeros.length);
+
+		for (int numero : numeros) {
+			suma += numero;
+		}
+		return suma;
 	}
 
-	// Mas de dos enteros
-	public int sumar(int x, int y, int z) {
-		return x + y;
+	/*
+	 * Flotantes public float sumar(float x, float y) { return x + y; }
+	 */
+
+	// Multiples parámetros flotantes
+
+	public float sumar(float... numeros) {
+		int suma = 0;
+
+		System.out.println(numeros.length);
+
+		for (float numero : numeros) {
+			suma += numero;
+		}
+		return suma;
 	}
 
-	// Flotantes
-	public float sumar(float x, float y) {
-		return x + y;
-	}
+	/*
+	 * Doubles public
+	 * 
+	 * double sumar(double x, double y) { return x + y; }
+	 * 
+	 */
 
-	// Doubles
-	public double sumar(double x, double y) {
-		return x + y;
+	// Multiples parámetros flotantes
+
+	public double sumar(double... numeros) {
+		int suma = 0;
+
+		System.out.println(numeros.length);
+
+		for (double numero : numeros) {
+			suma += numero;
+		}
+		return suma;
 	}
 
 	// Default
