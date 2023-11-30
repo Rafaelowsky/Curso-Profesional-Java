1960e7691ee7592acb95325bc7667749c21de5dc
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 20:18:46 -0600

    Añadí método default en interfazB

--- herenciaInterfaces/src/main/InterfazB.java
+++ herenciaInterfaces/src/main/InterfazB.java
@@ -2,4 +2,16 @@ package main;
 
 public interface InterfazB {
 	public abstract void mostrarMensaje();
+
+	/*
+	 * La palabra clave "default" se usa en interfaces de Java para definir métodos
+	 * con implementación por defecto. Estos métodos pueden ser utilizados por las
+	 * clases que implementan la interfaz sin requerir una implementación propia.
+	 * Esto facilita la adición de funcionalidades a las interfaces sin romper la
+	 * compatibilidad con implementaciones existentes.
+	 */
+
+	public default void saluda() {
+		System.out.println("Hola Mundo, desde interface!");
+	}
 }
---
+++ herenciaInterfaces/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		// Herencia de interfaces
+		
+		ClaseFinal claseFinal = new ClaseFinal();
+		claseFinal.mostrarMensaje();
+		
+		claseFinal.saluda();
+	}
+}
