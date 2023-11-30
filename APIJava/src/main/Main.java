e039708bd07c46c9870bfd394db7c5971780933a
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 20:35:01 -0600

    Añadí clase main.java para probar API math

---
+++ APIJava/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Main {
+	/*
+	 * Las APIs de Java son conjuntos de clases, interfaces, métodos y constantes
+	 * predefinidas que permiten a los desarrolladores interactuar con
+	 * funcionalidades específicas. Estas APIs ofrecen una forma estructurada y
+	 * documentada de utilizar las capacidades del lenguaje, como manejo de
+	 * colecciones, operaciones de entrada/salida, manipulación de cadenas, entre
+	 * otros. Facilitan el desarrollo al proveer herramientas ya implementadas y
+	 * optimizadas, reduciendo la necesidad de crear código desde cero.
+	 */
+
+	public static void main(String[] args) {
+		// Math.PI nos devuelve el valor de pi
+		System.out.println(Math.PI);
+		
+		// Math.max nos devuelve el valor que sea mayor de dos 
+		System.out.println(Math.max(100, 200));
+	}
+}
