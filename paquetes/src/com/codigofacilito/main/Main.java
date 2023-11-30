b51e0552bdeb743d706164e22a4a9d1b28a7491a
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 21:00:31 -0600

    Corregí error en importación de clase

--- paquetes/src/com/codigofacilito/main/Main.java
+++ paquetes/src/com/codigofacilito/main/Main.java
@@ -1,17 +1,16 @@
+package com.codigofacilito.main;
 /*
 Nombrar paquetes en Java: Utiliza nombres significativos y descriptivos en minúsculas, como com.tuempresa.modulo.funcionalidad.
 Evita nombres genéricos o excesivamente largos para mantener la claridad y la comprensión del código.
 */
 
-package com.codigofacilito.main;
-
+import com.codigofacilito.cursos.*;
 // Con esto importamos la clase dentro de otro paquete para poder ocuparla dentro de esta clase
 // Podemos importar todas las clases si en vez de la clase ponemos un *
-import com.codigofacilito.cursos.Java;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		// Aqui podemos comprobar como es que si podemos ocupar una clase que esta
 		// dentro de otro paquete y otra clase
 		Java java = new Java();
