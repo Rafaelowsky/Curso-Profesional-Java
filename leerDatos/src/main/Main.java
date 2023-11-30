bad228eefeca1229118a8a0539af9b924531652d
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 21:31:14 -0600

    Añadí programa para tomar datos

---
+++ leerDatos/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Instancia
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("¿Cual es tu nombres?");
+		
+		// Con .nextLine() vamos a tomar los datos que ingreso el usuario en la siguiente linea
+		String nombre = sc.nextLine();
+		System.out.println("Hola " + nombre);
+	}
+}
---
+++ leerDatos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rzava
+ *
+ */
+module leerDatos {
+}
