6f4e4522bd6df9dc8c5be240f27b3e042e8bb0f9
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-29 20:12:18 -0600

    Implementé herencia de interfaces

---
+++ herenciaInterfaces/src/main/ClaseFinal.java
@@ -0,0 +1,10 @@
+package main;
+
+public class ClaseFinal implements InterfazB {
+
+	@Override
+	public void mostrarMensaje() {
+		System.out.println("Hola");
+	}
+
+}
---
+++ herenciaInterfaces/src/main/InterfazB.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface InterfazB {
+	public abstract void mostrarMensaje();
+}
